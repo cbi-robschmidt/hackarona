b0e90c704d7bb84fbfaf9f66285adb60d33bafda
Rob Schmidt <rob.schmidt@cbrands.com>
2020-07-06 16:19:01 -0500

    Added AWS Lambda integration for image->text conversion

--- catalog/views.py
+++ catalog/views.py
@@ -5,6 +5,10 @@ from django.views.generic import ListView, DetailView
 
 from scanner.models import ScanPhoto
 
+import os
+import requests
+import json
+
 # Create your views here.
 class IndexView(ListView):
     queryset = ScanPhoto.objects.all()
@@ -13,3 +17,12 @@ class IndexView(ListView):
 class ResultsView(DetailView):
     model = ScanPhoto
     template_name = 'results.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ResultsView, self).get_context_data(**kwargs)
+        scan_instance = ScanPhoto.objects.get(uuid=self.kwargs['pk']).scan
+        imagename = os.path.basename(scan_instance.name)
+        resp = requests.get('https://0y0h6tom4a.execute-api.us-east-1.amazonaws.com/get-text?filename={}'.format(imagename))
+        textract_list = resp.json()
+        context['words'] = textract_list
+        return context
