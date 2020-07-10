8d9eeff321e7b6b1d8e0c4acb4f58ca3f7a3b04b
Rob Schmidt <rob.schmidt@cbrands.com>
2020-07-10 14:52:29 -0500

    Recipes AJAX call added

--- static/catalog_styling/get_text.js
+++ static/catalog_styling/get_text.js
@@ -1,13 +1,14 @@
 const GET_TEXT_URL = 'https://0y0h6tom4a.execute-api.us-east-1.amazonaws.com/get-text';
 const GET_RECIPES_URL = 'https://0y0h6tom4a.execute-api.us-east-1.amazonaws.com/getRecipe';
 
+var words
+
 $(document).ready(function gatherWords() {
     const imageNameHeader = $('.obj-name');
     const wordContainer = $("#word-container");
     const loadingText = $("#words-loading");
 
     const imageUrl = GET_TEXT_URL + "?filename=" + imageNameHeader.text().trim();
-    console.log('Finding words with url ' + imageUrl);
     $.get(imageUrl, function(data, status) {
         loadingText.hide();
 
@@ -15,6 +16,23 @@ $(document).ready(function gatherWords() {
         resp = JSON.parse(data);
         $.each(resp['words'], function(index, value) {
             wordContainer.append(`<div>${value}</div`);
+        });
+
+        const recipeContainer = $('#recipe-container');
+        const recipeStatus = $('#recipe-status');
+        $.ajax({
+            url: GET_RECIPES_URL, 
+            type: "POST",
+            contentType: 'application/json',
+            data: JSON.stringify({query: resp['words']}), 
+            success: function(data) {
+                recipeStatus.hide();
+                recipes = JSON.parse("{\"body\":"+data.body+"}");
+                console.log(recipes);
+                $.each(recipes.body, function(index, value) {
+                    recipeContainer.append(`<a href=${value['webImage']}><h2>${value['name']}</h2><br /><img src=${value['webImage']} /></a>`);
+                });
+            }
         });
     });
 });
