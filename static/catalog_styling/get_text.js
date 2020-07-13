fb738a30cc4c9b450b70913fdc64da590e341164
Rob Schmidt <67714664+cbi-robschmidt@users.noreply.github.com>
2020-07-13 10:11:46 -0500

    Add jQuery and AJAX AWS API calls (#11)
    * Added jQuery and cleaned AJAX code
    * Recipes AJAX call added
    * Removed unneeded code from catalog/views.py
    * Preserve 'no recipes found' message on empty recipe array
    * Removed views.py merge conflict keyword
    
    Co-authored-by: Spenser Dewey <SDEWEY@CWC.cbi.net>

---
+++ static/catalog_styling/get_text.js
@@ -0,0 +1,39 @@
+const GET_TEXT_URL = 'https://0y0h6tom4a.execute-api.us-east-1.amazonaws.com/get-text';
+const GET_RECIPES_URL = 'https://0y0h6tom4a.execute-api.us-east-1.amazonaws.com/getRecipe';
+
+var words
+
+$(document).ready(function gatherWords() {
+    const imageNameHeader = $('.obj-name');
+    const wordContainer = $("#word-container");
+    const loadingText = $("#words-loading");
+
+    const imageUrl = GET_TEXT_URL + "?filename=" + imageNameHeader.text().trim();
+    $.get(imageUrl, function(data, status) {
+        loadingText.hide();
+
+        console.log(data);
+        resp = JSON.parse(data);
+        $.each(resp['words'], function(index, value) {
+            wordContainer.append(`<div>${value}</div`);
+        });
+
+        const recipeContainer = $('#recipe-container');
+        const recipeStatus = $('#recipe-status');
+        $.ajax({
+            url: GET_RECIPES_URL, 
+            type: "POST",
+            contentType: 'application/json',
+            data: JSON.stringify({query: resp['words']}), 
+            success: function(data) {
+                recipes = JSON.parse("{\"body\":"+data.body+"}");
+                console.log(recipes.body);
+                if(recipes.body.length != 0)
+                    recipeStatus.hide();
+                $.each(recipes.body, function(index, value) {
+                    recipeContainer.append(`<a href=${value['webImage']}><h2>${value['name']}</h2><br /><img src=${value['webImage']} /></a>`);
+                });
+            }
+        });
+    });
+});
