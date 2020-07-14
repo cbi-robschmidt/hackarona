90e2574fc9b9a0a31ebcbb7b9b6d2ff837458582
Rob Schmidt <67714664+cbi-robschmidt@users.noreply.github.com>
2020-07-14 09:26:27 -0500

    Removed logic from client-side AJAX (#14)
    * Server side view now handles AJAX logic
    * get_text.js calls new view through $.getJSON and parses returned data
    * Header added for recipe section in results.html

--- scanner/views.py
+++ scanner/views.py
@@ -1,8 +1,14 @@
 from django.views.generic.edit import FormView
 from django.shortcuts import render, redirect
 
+from django.views.generic.base import View
+from django.http import JsonResponse
+
 from .forms import UploadScanForm
 from .models import ScanPhoto
+
+import requests
+import json
 
 # Create your views here.
 def scan_form_view(request):
@@ -16,3 +22,61 @@ def scan_form_view(request):
         return render(request, 'scan.html', {
             'form': form
         })
+    
+class GetScanData(View):
+    ''' 
+        API call which returns a JSON formatted dict
+        containing the list of words detected and the
+        related recipes
+    '''
+    # restrict the http allowed methods to only GET 
+    http_method_names = ['get']
+
+    def get(self, request, *args, **kwargs):
+        # api call urls
+        textract_url = 'https://0y0h6tom4a.execute-api.us-east-1.amazonaws.com/get-text'
+        recipes_url = 'https://0y0h6tom4a.execute-api.us-east-1.amazonaws.com/getRecipe'
+        spellcheck_url = 'https://api.datamuse.com/words'
+
+        # filename is passed as get parameter to the view
+        filename = request.GET.get('filename', None)
+        if not filename:
+            return JsonResponse({'message': 'No filename'})
+
+        # get the AWS Textract detection objects
+        textract_response = requests.get(textract_url, params={'filename': filename})
+        words = []
+        ret_words = []
+        textract = textract_response.json()['words']
+        # process each returned word
+        for entry in textract:
+            # avoid small words to increase specificity 
+            if len(entry['word']) < 4:
+                continue
+
+            # words with a confidence above 85 are printed as-is
+            if entry['score'] > 85:
+                words.append(entry['word'].upper())
+                ret_words.append({'word': entry['word'].upper(), 'score': entry['score']})
+            # words with a confidence of 70 < x <= 85 are autocorrected
+            elif entry['score'] > 70:
+                # attempt to correct
+                corrections = requests.get(spellcheck_url, params={'sp': entry['word']}).json()
+                # if a non-zero amount of corrections have been returned, add the first
+                # since the list is ordered by score/confidence descending
+                if corrections:
+                    words.append(corrections[0]['word'].upper())
+                    ret_words.append({'word': entry['word'].upper(), 'score': entry['score']})
+
+        # send the list of words to the recipe AWS Lambda function
+        getrecipe_response = requests.post(recipes_url, json={'query': words})
+        getrecipe_response = getrecipe_response.json()
+        # load the recipe list from the recipe response's body element
+        recipe_body = json.loads(getrecipe_response['body'])
+        recipes = []
+        # append each entry as an element in a list
+        for entry in recipe_body:
+            recipes.append(entry)
+        
+        # return the JSON formatted response containing the words and recipes
+        return JsonResponse({'words': ret_words, 'recipes': recipes})
--- static/catalog_styling/get_text.js
+++ static/catalog_styling/get_text.js
@@ -1,44 +1,31 @@
-const GET_TEXT_URL = 'https://0y0h6tom4a.execute-api.us-east-1.amazonaws.com/get-text';
-const GET_RECIPES_URL = 'https://0y0h6tom4a.execute-api.us-east-1.amazonaws.com/getRecipe';
-
-var words
+const SCAN_DATA_URL = '/scan/get-scan-data/';
 
 $(document).ready(function gatherWords() {
     const imageNameHeader = $('.obj-name');
     const wordContainer = $("#word-container");
     const loadingText = $("#words-loading");
 
-    const imageUrl = GET_TEXT_URL + "?filename=" + imageNameHeader.text().trim();
-    $.get(imageUrl, function(data, status) {
+    const scanUrl = SCAN_DATA_URL + "?filename=" + imageNameHeader.text().trim();
+
+    $.getJSON(scanUrl, function(data) {
+        // hide the loading notification
         loadingText.hide();
 
-        console.log(data);
-        resp = JSON.parse(data);
-        var words = [];
-        for (var i = 0; i < resp['words'].length; i++) {
-            const value = resp['words'][i];
-            if (value.score > 70 && value.word.length > 3) {
-                wordContainer.append(`<div>${value.word}</div`);
-                words.push(value.word);
-            }
-        }
-
-        const recipeContainer = $('#recipe-container');
-        const recipeStatus = $('#recipe-status');
-        $.ajax({
-            url: GET_RECIPES_URL, 
-            type: "POST",
-            contentType: 'application/json',
-            data: JSON.stringify({query: words}), 
-            success: function(data) {
-                recipes = JSON.parse("{\"body\":"+data.body+"}");
-                console.log(recipes.body);
-                if(recipes.body.length != 0)
-                    recipeStatus.hide();
-                $.each(recipes.body, function(index, value) {
-                    recipeContainer.append(`<a class="catalog-item" href=${value['webImage']}><h3>${value['name']}</h3><img src=${value['webImage']} /></a>`);
-                });
-            }
+        // append each word detected to the container
+        $.each(data.words, function (index, value) {
+            value.score = parseFloat(value.score).toFixed(1);
+            wordContainer.append(`<div>${value.word}<br /><i>confidence: ${value.score}</i></div`);
         });
+
+        // if a non-zero amount of recipes found, display them
+        if(data.recipes.length > 0) {
+            $('#recipe-status').hide();
+
+            $.each(data.recipes, function (index, value) {
+                const recipeContainer = $('#recipe-container');
+
+                recipeContainer.append(`<a href=${value.webImage}><h2>${value.name}</h2><br /><img src=${value.webImage} /></a>`);
+            });
+        }
     });
 });
