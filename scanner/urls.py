aa60e67e3168b2c0c27a3e5cc9a2635bdf68edfd
Rob Schmidt <rob.schmidt@cbrands.com>
2020-07-14 08:54:29 -0500

    Removed logic from client-side AJAX * Server side view now handles AJAX logic * get_text.js calls new view through $.getJSON and parses returned data
    Header added for recipe section in results.html

--- scanner/urls.py
+++ scanner/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import scan_form_view
+from .views import scan_form_view, GetScanData
 
 urlpatterns = [
     path('', scan_form_view, name='scan'),
+    path('get-scan-data/', GetScanData.as_view(), name='get-scan-data'),
 ]
