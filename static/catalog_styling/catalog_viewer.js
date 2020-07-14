e962aa1bcedf6ecddfdda737829036cb1d3edbbf
Rob Schmidt <rob.schmidt@cbrands.com>
2020-07-14 11:01:39 -0500

    Moved to new recipes API which will account for a b64 encoded body
    fixed catalog viewer to use new recipes API

--- static/catalog_styling/catalog_viewer.js
+++ static/catalog_styling/catalog_viewer.js
@@ -1,4 +1,4 @@
-const GET_RECIPES_URL = 'https://0y0h6tom4a.execute-api.us-east-1.amazonaws.com/getRecipe';
+const GET_RECIPES_URL = 'https://0y0h6tom4a.execute-api.us-east-1.amazonaws.com/get-recipes';
 let page = 1;
 $(document).ready(getRecipes);
 
@@ -11,13 +11,14 @@ function getRecipes() {
         url: GET_RECIPES_URL, 
         type: "POST",
         contentType: 'application/json',
-        data: JSON.stringify({query: [query], page}), 
+        data: JSON.stringify({query: [query], page: page}), 
         success: function(data) {
-            const recipes = JSON.parse("{\"body\":"+data.body+"}");
-            if(recipes.body.length == 0) {
+            console.log(data);
+            const recipes = JSON.parse(data);
+            if(recipes.length == 0) {
                 $("#moreButton").hide();
             }
-            $.each(recipes.body, function(index, value) {
+            $.each(recipes, function(index, value) {
                 catalog.append(`<a class="catalog-item" href=${value['webImage']}><h3>${value['name']}</h3><br /><img src=${value['webImage']} /></a>`);
             });
         }
